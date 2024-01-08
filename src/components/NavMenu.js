186c20a8c7b2ed1ab8119ac9aa2c55619ed287a5
Jurek <jerzykomorowski77@gmail.com>
2024-01-08 18:53:49 +0100

    menu znika po kliknięciu w Link dot dużego ekranu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,73 +1,116 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import './NavMenu.css';
 
 const NavMenu = ({ handleCloseMobileMenu, mobileMenu }) => {
+    const [isMenuActive, setMenuActive] = useState(false);
+
+    console.log(mobileMenu);
+
+    const toggleActiveState = () => {
+        setMenuActive(!isMenuActive);
+    };
+
+    const handleLinkClick = () => {
+        handleCloseMobileMenu();
+        setMenuActive(false);
+
+    };
+
     return (
         <nav className='nav_menu'>
-            <ul className={!mobileMenu ? 'nav_menu__menu' : 'nav_menu__menu active'}>
-                <li className='nav_menu__menu-item'><Link className="nav_menu-link">Tabelka
+            <ul className={!mobileMenu ? 'nav_menu__menu' : 'nav_menu__menu active'}  >
+                <li className='nav_menu__menu-item'><Link className="nav_menu-link" onClick={toggleActiveState}>Tabelka
                 </Link>
-                    <div className="nav_menu-dropdown__menu">
-                        <ul className='nav_menu__menu-dropmenu'>
-                            <li className='nav_menu-item-dropmenu'><Link to="/" className="nav_menu-link" onClick={handleCloseMobileMenu}>Aktualne
+                    {/* <div className="nav_menu-dropdown__menu"> */}
+                    <div className={isMenuActive ? 'nav_menu-dropdown__menu' : 'nav_menu-dropdown__menu-disabled'} onMouseLeave={() => setMenuActive(false)}>
+                        <ul className='nav_menu__menu-dropmenu' >
+                            <li className='nav_menu-item-dropmenu'><Link to="/" className="nav_menu-link" onClick={handleLinkClick}>Aktualne
                             </Link></li>
-                            <li className='nav_menu-item-dropmenu'><a>Archiwum</a></li>
-                            <li className='nav_menu-item-dropmenu'><a>Kancelaria</a>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Archiwum
+                            </Link></li>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Kancelaria
+                            </Link>
                             </li>
-                            <li className='nav_menu-item-dropmenu'><a>Kpl dane</a>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Kpl dane
+                            </Link>
                             </li>
                         </ul>
                     </div></li>
-                <li className='nav_menu__menu-item'><a>Dodatki</a>
-                    <div className="nav_menu-dropdown__menu">
+                {/* <li className='nav_menu__menu-item'><Link className="nav_menu-link">Dodatki */}
+                <li className='nav_menu__menu-item'><Link className="nav_menu-link" onClick={toggleActiveState}>Dodatki
+                </Link>
+                    {/* <div className="nav_menu-dropdown__menu"> */}
+                    <div className={isMenuActive ? 'nav_menu-dropdown__menu' : 'nav_menu-dropdown__menu-disabled'} onMouseLeave={() => setMenuActive(false)}>
                         <ul className='nav_menu__menu-dropmenu'>
-                            <li className='nav_menu-item-dropmenu'><a>Mailing</a></li>
-                            <li className='nav_menu-item-dropmenu'><Link to="/contacts" className="nav_menu-link" onClick={handleCloseMobileMenu}>Kontakty
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Mailing
+                            </Link></li>
+                            <li className='nav_menu-item-dropmenu'><Link to="/contacts" className="nav_menu-link" onClick={handleLinkClick}>Kontakty
                             </Link></li>
 
                         </ul>
                     </div>
                 </li>
-                <li className='nav_menu__menu-item'><a>Raporty</a>
-                    <div className="nav_menu-dropdown__menu">
+                <li className='nav_menu__menu-item'><Link className="nav_menu-link" onClick={toggleActiveState}>Raporty
+                </Link>
+                    {/* <div className="nav_menu-dropdown__menu"> */}
+                    <div className={isMenuActive ? 'nav_menu-dropdown__menu' : 'nav_menu-dropdown__menu-disabled'} onMouseLeave={() => setMenuActive(false)}>
                         <ul className='nav_menu__menu-dropmenu'>
-                            <li className='nav_menu-item-dropmenu'><a>Zbiorczy działu</a></li>
-                            <li className='nav_menu-item-dropmenu'><a>Pracownika</a></li>
-                            <li className='nav_menu-item-dropmenu'><a>Test
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Zbiorczy
+                            </Link></li>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Pracownika
+                            </Link></li>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link">Test
+
                                 <i className='fas fa-caret-right' ></i>
-                            </a>
+                            </Link>
                                 <div className="nav_menu-dropdown__menu--side">
                                     <ul className='nav_menu__menu--side'>
-                                        <li className='nav_menu-item-dropmenu--side'><a>Test-1</a></li>
-                                        <li className='nav_menu-item-dropmenu--side'><a>Test-2</a></li>
-                                        <li className='nav_menu-item-dropmenu--side'><a>Test-3</a></li>
-                                        <li className='nav_menu-item-dropmenu--side'><a>Test-4</a></li>
+                                        <li className='nav_menu-item-dropmenu--side'><Link className="nav_menu-link" onClick={handleLinkClick}>Test-1
+                                        </Link></li>
+                                        <li className='nav_menu-item-dropmenu--side'><Link className="nav_menu-link" onClick={handleLinkClick}>Test-2
+                                        </Link></li>
+                                        <li className='nav_menu-item-dropmenu--side'><Link className="nav_menu-link" onClick={handleLinkClick}>Test-3
+                                        </Link></li>
+                                        <li className='nav_menu-item-dropmenu--side'><Link className="nav_menu-link" onClick={handleLinkClick}>Test-4
+                                        </Link></li>
                                     </ul>
                                 </div>
                             </li>
                         </ul>
                     </div>
                 </li>
-                <li className='nav_menu__menu-item'><a>Ustawienia</a>
-                    <div className="nav_menu-dropdown__menu">
+                <li className='nav_menu__menu-item'><Link className="nav_menu-link" onClick={toggleActiveState}>Ustawienia
+                </Link>
+                    {/* <div className="nav_menu-dropdown__menu"> */}
+                    <div className={isMenuActive ? 'nav_menu-dropdown__menu' : 'nav_menu-dropdown__menu-disabled'} onMouseLeave={() => setMenuActive(false)}>
                         <ul className='nav_menu__menu-dropmenu'>
-                            <li className='nav_menu-item-dropmenu'><a>Kolumny tabeli</a></li>
-                            <li className='nav_menu-item-dropmenu'><a>Kontakty</a></li>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Kolumny tabeli
+                            </Link></li>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Ustawienia
+                            </Link></li>
                         </ul>
                     </div></li>
-                <li className='nav_menu__menu-item'><a>Użytkownik</a><div className="nav_menu-dropdown__menu">
-                    <ul className='nav_menu__menu-dropmenu'>
-                        <li className='nav_menu-item-dropmenu'><a>Dodaj użytkownika</a></li>
-                        <li className='nav_menu-item-dropmenu'><a>Zmień uprawnienia</a></li>
-                        <li className='nav_menu-item-dropmenu'><a>Zmień hasło</a>
-                        </li>
-                        <li className='nav_menu-item-dropmenu'><a>Wyloguj</a></li>
-                    </ul>
-                </div></li>
+                <li className='nav_menu__menu-item'><Link className="nav_menu-link" onClick={toggleActiveState}>Użytkownik
+                </Link>
+                    {/* <div className="nav_menu-dropdown__menu"> */}
+                    <div className={isMenuActive ? 'nav_menu-dropdown__menu' : 'nav_menu-dropdown__menu-disabled'} onMouseLeave={() => setMenuActive(false)}>
+                        <ul className='nav_menu__menu-dropmenu'>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Dodaj użytkownika
+                            </Link></li>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Zmień uprawnienia
+                            </Link></li>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Zmień hasło
+                            </Link>
+                            </li>
+                            <li className='nav_menu-item-dropmenu'><Link className="nav_menu-link" onClick={handleLinkClick}>Wyloguj
+                            </Link></li>
+                        </ul>
+                    </div></li>
             </ul>
-        </nav>
+        </nav >
     );
 };
 
 export default NavMenu;
+;
