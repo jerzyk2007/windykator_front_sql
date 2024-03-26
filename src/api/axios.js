36fffd722be6947a4d014bc6531b71341f47fd59
Jurek <jerzykomorowski77@gmail.com>
2024-03-26 18:16:02 +0100

    testy nowego serwera

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const BASE_URL = 'http://localhost:3500';
-const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'http://localhost:3000';
 // const BASE_URL = 'http://localhost';
 const BASE_URL = 'https://server.raportbl.eu';
 // // const BASE_URL = 'http://10.8.20.11:3000:3500';
