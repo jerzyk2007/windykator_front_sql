e36093e4ba2e1864ec6cc5b62c0825b8230171ce
Komorowski <jerzykomorowski77@gmail.com>
2024-04-15 16:45:00 +0200

    komponent FKItemSettings do ustawiania danych zrobione: możliwość wyboru z listy, odczytu z DB ostylowanie jeszcze nie ma zapisu do DB

--- src/components/FKRaport/FKDataSettings.js
+++ src/components/FKRaport/FKDataSettings.js
@@ -1,52 +1,118 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useAxiosPrivateIntercept from "../hooks/useAxiosPrivate";
 import PleaseWait from "../PleaseWait";
 import useData from "../hooks/useData";
+import FKItemSettings from "./FKItemsData/FKItemSettings";
 import "./FKDataSettings.css";
 
 const FKDataSettings = () => {
   const axiosPrivateIntercept = useAxiosPrivateIntercept();
   const { pleaseWait, setPleaseWait } = useData();
 
-  // const [errFKAging, setErrFKAging] = useState("");
+  const [data, setData] = useState([]);
+  const [raportDep, setRaportDep] = useState([]);
+  const [mergeDep, setMergeDep] = useState([]);
+  const [testData, setTestData] = useState([]);
 
-  // const handleSendFile = async (e, type) => {
-  //   console.log(type);
+  const uniqeDep = [
+    "D001",
+    "D002",
+    "D003",
+    "D004",
+    "D005",
+    "D006",
+    "D007",
+    "D008",
+    "D009",
+    "D014",
+    "D017",
+    "D027",
+    "D031",
+    "D034",
+    "D536",
+    "D537",
+    "KSIĘGOWOŚĆ",
+  ];
 
-  //   setPleaseWait(true);
-  //   const file = e.target.files[0];
-  //   if (!file) return console.log("Brak pliku");
-  //   if (!file.name.endsWith(".xlsx")) {
-  //     setErrFKAging("Akceptowany jest tylko plik z rozszerzeniem .xlsx");
-  //     return;
-  //   }
-  //   try {
-  //     const formData = new FormData();
-  //     formData.append("excelFile", file);
+  const getData = async () => {
+    try {
+      setPleaseWait(true);
+      const result = await axiosPrivateIntercept.get("/fk/get-fksettings-data");
+      setData(result.data);
+      const uniqueDep = await axiosPrivateIntercept.get("/fk/get-uniques-dep");
 
-  //     const response = await axiosPrivateIntercept.post(
-  //       `/fk/send-documents/${type}`,
-  //       formData,
-  //       {
-  //         headers: {
-  //           "Content-Type": "multipart/form-data",
-  //         },
-  //       }
-  //     );
+      setRaportDep(uniqueDep.data.departments);
 
-  //     if (type === "aging") {
-  //       setErrFKAging("Dokumenty zaktualizowane.");
-  //     }
+      setPleaseWait(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  //     setPleaseWait(false);
-  //   } catch (error) {
-  //     if (type === "sharepoint") {
-  //       setErrFKAging("Błąd aktualizacji dokumentów.");
-  //     }
-  //     console.error("Błąd przesyłania pliku:", error);
-  //     setPleaseWait(false);
-  //   }
-  // };
+  const itemsArray = uniqeDep.map((dep, index) => {
+    return (
+      <FKItemSettings
+        key={index}
+        id={index}
+        dep={dep}
+        dataItem={testData ? testData[index] : {}}
+        settings={data}
+        style="exist"
+        testData={testData}
+      />
+    );
+  });
+
+  // funkcja która ma sprawdzić które działy są w pliku przygotowanym do generowania raportu, a które działy sa zapisane a nie będą uzyte w raporcie
+  const checkDep = (depDB, depRaport) => {
+    const resultArray = [];
+
+    depDB.forEach((item) => {
+      const obj = { dep: item, exist: false };
+      resultArray.push(obj);
+    });
+
+    resultArray.forEach((item) => {
+      if (uniqeDep.includes(item.dep)) {
+        item.exist = true;
+      }
+    });
+    // depRaport.forEach((item) => {
+    //   if (!depDB.includes(item)) {
+    //     resultArray.push({ dep: item, exist: true });
+    //   }
+    // });
+
+    // setMergeDep(resultArray);
+    // console.log(resultArray);
+    // console.log(resultArray);
+  };
+
+  useEffect(() => {
+    if (data?.departments && raportDep) {
+      checkDep(data.departments, raportDep);
+    }
+    const createDataDep = uniqeDep.map((item) => {
+      const test = {
+        department: item,
+        localization: "",
+        area: "",
+        owner: [""],
+        guardian: [""],
+      };
+
+      return test;
+    });
+    setTestData(createDataDep);
+  }, [data, raportDep]);
+
+  useEffect(() => {
+    // console.log(testData);
+  }, [testData]);
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
@@ -59,26 +125,25 @@ const FKDataSettings = () => {
           </section>
           <section className="fk_data_settings__container">
             <section className="fk_data_settings__container-item">
+              <section className="fk_data_settings-counter__container">
+                <span className="fk_data_settings-counter">Lp</span>
+              </section>
               <span className="fk_data_settings-department">Dział</span>
               <span className="fk_data_settings-localization">Lokalizacja</span>
               <span className="fk_data_settings-area">Obszar</span>
               <span className="fk_data_settings-owner">Owner</span>
-              <span className="fk_data_settings-owner">Opiekun</span>
+              <span className="fk_data_settings-guardian">Opiekun</span>
             </section>
 
-            <section className="fk_data_settings__container-item">
-              <span>Wiekowanie - plik księgowość</span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </section>
-
-            <section className="fk_data_settings__container-item">
-              <span>Rozrachunki - przelewy bankowe</span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </section>
+            {/* <section className="fk_data_settings__container-item">
+              <span className="fk_data_settings-counter">LP</span>
+              <span className="fk_data_settings-department">Dział</span>
+              <span className="fk_data_settings-localization">Lokalizacja</span>
+              <span className="fk_data_settings-area">Obszar</span>
+              <span className="fk_data_settings-owner">Owner</span>
+              <span className="fk_data_settings-guardian">Opiekun</span>
+            </section> */}
+            {itemsArray}
           </section>
         </section>
       )}
---
+++ src/components/FKRaport/FKItemsData/FKItemSettings.js
@@ -0,0 +1,282 @@
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+const FKItemSettings = ({ id, dep, dataItem, settings, style }) => {
+  const [settingsItem, setSettingsItem] = useState([]);
+  const [itemsValue, setItemsValue] = useState({});
+  const [activeSave, setActiveSave] = useState(false);
+
+  //   console.log(dataItem);
+
+  const handleLocalization = (e, info) => {
+    // console.log(info);
+    const newValue = e.target.value;
+    setItemsValue((prev) => {
+      return {
+        ...prev,
+        [info]: newValue,
+      };
+    });
+  };
+
+  const localizationItems = settingsItem.localization?.map((loc, index) => {
+    return (
+      <MenuItem value={loc} key={index}>
+        {loc}
+      </MenuItem>
+    );
+  });
+
+  const areaItems = settingsItem.areas?.map((area, index) => {
+    return (
+      <MenuItem value={area} key={index}>
+        {area}
+      </MenuItem>
+    );
+  });
+
+  // usuwa nadplanowego ownera, ale zawsze zostanie jeden
+  const handleDeleteOwner = (id) => {
+    const updateOwner = [...itemsValue.owner];
+
+    updateOwner.splice(id, 1);
+    setItemsValue((prev) => {
+      return {
+        ...prev,
+        owner: updateOwner,
+      };
+    });
+  };
+
+  //dodaje dodatkowego ownera
+  const handleAddOwner = () => {
+    const updateOwner = [...itemsValue.owner, ""];
+    setItemsValue((prev) => {
+      return {
+        ...prev,
+        owner: updateOwner,
+      };
+    });
+  };
+
+  // aktualizuję ownera po wyborze
+  const handleUpdateOwner = (e, id) => {
+    const newOwner = e.target.value;
+    const ownArray = [...itemsValue.owner];
+    if (ownArray.includes(newOwner)) return;
+    ownArray[id] = newOwner;
+
+    setItemsValue((prev) => {
+      return {
+        ...prev,
+        owner: ownArray,
+      };
+    });
+  };
+
+  const ownerItems = itemsValue?.owner?.map((own, index) => {
+    const menuItems = settingsItem.owners?.map((ownItem, id) => {
+      return (
+        <MenuItem value={ownItem} key={id}>
+          {ownItem}
+        </MenuItem>
+      );
+    });
+
+    return (
+      <section className="fk_data_settings-owner-many" key={index}>
+        <Box className="fk_data_settings-owner__container">
+          <FormControl fullWidth>
+            <InputLabel id="simple-select-label">Owner</InputLabel>
+            <Select
+              labelId="simple-select-label"
+              id="simple-select"
+              value={itemsValue?.owner[index] ? itemsValue.owner[index] : ""}
+              label="Obszar"
+              onChange={(e) => handleUpdateOwner(e, index, "owner")}
+            >
+              {menuItems}
+            </Select>
+          </FormControl>
+        </Box>
+        {index === 0 && (
+          <i
+            className="fa-solid fa-plus fk_data_settings--fa-plus"
+            onClick={handleAddOwner}
+          ></i>
+        )}
+        {index !== 0 && (
+          <i
+            className="fa-solid fa-minus fk_data_settings--fa-minus"
+            onClick={() => {
+              handleDeleteOwner(index);
+            }}
+          ></i>
+        )}
+      </section>
+    );
+  });
+
+  // usuwa nadplanowego ownera, ale zawsze zostanie jeden
+  const handleDeleteGuardian = (id) => {
+    const updateGuardian = [...itemsValue.guardian];
+
+    updateGuardian.splice(id, 1);
+    setItemsValue((prev) => {
+      return {
+        ...prev,
+        guardian: updateGuardian,
+      };
+    });
+  };
+
+  //dodaje dodatkowego ownera
+  const handleAddGuardian = () => {
+    const updateGuardian = [...itemsValue.guardian, ""];
+    setItemsValue((prev) => {
+      return {
+        ...prev,
+        guardian: updateGuardian,
+      };
+    });
+  };
+
+  // aktualizuję ownera po wyborze
+  const handleUpdateGuardian = (e, id) => {
+    const newGuardian = e.target.value;
+    const guardArray = [...itemsValue.guardian];
+    if (guardArray.includes(newGuardian)) return;
+    guardArray[id] = newGuardian;
+
+    setItemsValue((prev) => {
+      return {
+        ...prev,
+        guardian: guardArray,
+      };
+    });
+  };
+
+  const guardianItems = itemsValue?.guardian?.map((guard, index) => {
+    const menuItems = settingsItem.guardians?.map((guardItem, id) => {
+      return (
+        <MenuItem value={guardItem} key={id}>
+          {guardItem}
+        </MenuItem>
+      );
+    });
+
+    return (
+      <section className="fk_data_settings-owner-many" key={index}>
+        <Box className="fk_data_settings-owner__container">
+          <FormControl fullWidth>
+            <InputLabel id="simple-select-label">Opiekun</InputLabel>
+            <Select
+              labelId="simple-select-label"
+              id="simple-select"
+              value={
+                itemsValue?.guardian[index] ? itemsValue.guardian[index] : ""
+              }
+              label="Obszar"
+              onChange={(e) => handleUpdateGuardian(e, index, "guardian")}
+            >
+              {menuItems}
+            </Select>
+          </FormControl>
+        </Box>
+        {index === 0 && (
+          <i
+            className="fa-solid fa-plus fk_data_settings--fa-plus"
+            onClick={handleAddGuardian}
+          ></i>
+        )}
+        {index !== 0 && (
+          <i
+            className="fa-solid fa-minus fk_data_settings--fa-minus"
+            onClick={() => {
+              handleDeleteGuardian(index);
+            }}
+          ></i>
+        )}
+      </section>
+    );
+  });
+
+  useEffect(() => {
+    setSettingsItem(settings);
+    setItemsValue(dataItem);
+  }, [settings, dataItem]);
+
+  useEffect(() => {
+    if (
+      itemsValue.localization &&
+      itemsValue.area &&
+      !itemsValue.owner.includes("") &&
+      !itemsValue.guardian.includes("")
+    ) {
+      setActiveSave(true);
+    } else {
+      setActiveSave(false);
+    }
+    console.log(itemsValue);
+  }, [itemsValue]);
+
+  return (
+    <section className="fk_data_settings__container-item">
+      <section className="fk_data_settings-counter__container">
+        <span
+          className="fk_data_settings-counter"
+          style={style === "exist" ? { backgroundColor: "#aeffe4" } : null}
+        >
+          {id + 1}
+        </span>
+        {activeSave && (
+          <i className="fas fa-save fk_data_settings--fa-save"></i>
+        )}
+      </section>
+
+      <span className="fk_data_settings-department">
+        {itemsValue.department}
+      </span>
+      <section className="fk_data_settings-localization">
+        <Box>
+          <FormControl fullWidth>
+            <InputLabel id="simple-select-label">Lokalizacja</InputLabel>
+            <Select
+              labelId="simple-select-label"
+              id="simple-select"
+              value={itemsValue.localization ? itemsValue.localization : ""}
+              label="Lokalizacja"
+              onChange={(e) => handleLocalization(e, "localization")}
+            >
+              {localizationItems}
+            </Select>
+          </FormControl>
+        </Box>
+      </section>
+      <section className="fk_data_settings-area">
+        <Box>
+          <FormControl fullWidth>
+            <InputLabel id="simple-select-label">Obszar</InputLabel>
+            <Select
+              labelId="simple-select-label"
+              id="simple-select"
+              value={itemsValue.area ? itemsValue.area : ""}
+              label="Obszar"
+              onChange={(e) => handleLocalization(e, "area")}
+            >
+              {areaItems}
+            </Select>
+          </FormControl>
+        </Box>
+      </section>
+      <section className="fk_data_settings-owner">{ownerItems}</section>
+      <section className="fk_data_settings-owner">{guardianItems}</section>
+    </section>
+  );
+};
+
+export default FKItemSettings;
