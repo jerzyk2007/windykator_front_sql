e31073504b262bf71595c6d4285f3e57b17a4c51
jerzyk2007 <jerzykomorowski77@gmail.com>
2024-11-05 14:33:04 +0100

    axios 3000

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500";
-// const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3500";
+const BASE_URL = "http://localhost:3000";
 // const BASE_URL = 'http://localhost';
 // const BASE_URL = 'https://server.raportbl.eu';
 // const BASE_URL = "https://raportbl.krotoski.com:3000";
