9d33d5d8293da990a2e0ccaaae45b6bc286ee0c8
jerzyk2007 <jerzykomorowski77@gmail.com>
2025-08-20 14:05:06 +0200

    dodałem dodatkową opcję do EditDocActions, dla "Wybierz kancelarię TU" dodałem opcję "Windykacja Wewnętrzna"

--- src/components/table/EditDocActions.js
+++ src/components/table/EditDocActions.js
@@ -134,7 +134,7 @@ const EditDocActions = ({ rowData, setRowData, handleAddNote, roles }) => {
               }}
             >
               <option value="">BRAK</option>
-              <option value="WEWNĘTRZNA WINDYKACJA">WEWNĘTRZNA WINDYKACJA</option>
+              <option value="WINDYKACJA WEWNĘTRZNA">WINDYKACJA WEWNĘTRZNA</option>
               <option value="KANCELARIA KROTOSKI">KANCELARIA KROTOSKI</option>
               <option value="KRAUZE">KRAUZE</option>
               <option value="POSTEPOWANIE SANACYJNE">
