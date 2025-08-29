8b1df9f8d0bf65380beca4a143739324732c4885
jerzyk2007 <jerzykomorowski77@gmail.com>
2025-08-29 10:02:21 +0200

    porządek w kodzie

--- src/components/system_settings/AddDataFromFile.js
+++ src/components/system_settings/AddDataFromFile.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import useAxiosPrivateIntercept from "../hooks/useAxiosPrivate";
 import PleaseWait from "../PleaseWait";
+import { saveAs } from "file-saver"; 
 import "./AddDataFromFile.css";
 
 const AddDataFromFile = () => {
@@ -35,6 +36,62 @@ const AddDataFromFile = () => {
           },
         }
       );
+      if (type === "becared") {
+        setErrBecared("Dokumenty zaktualizowane.");
+      }
+      else if (type === "rubicon") {
+        setErrRubicon("Dokumenty zaktualizowane.");
+      }
+      else if (type === "random") {
+        setErrRandomFile("Dokumenty zaktualizowane.");
+      }
+
+    } catch (error) {
+      if (type === "becared") {
+        setErrBecared("Błąd aktualizacji dokumentów.");
+      }
+      else if (type === "rubicon") {
+        setErrRubicon("Błąd aktualizacji dokumentów.");
+      }
+      else if (type === "random") {
+        setErrRandomFile("Błąd aktualizacji dokumentów.");
+      }
+      console.error("Błąd przesyłania pliku:", error);
+    } finally {
+      setPleaseWait(false);
+    }
+  };
+  const handleSendFileBL2 = async (e, type) => {
+    const file = e.target.files[0];
+    if (!file) return console.error("Brak pliku");
+    if (!file.name.endsWith(".xlsx") && !file.name.endsWith(".xls")) {
+      setErrFile("Akceptowany jest tylko plik z rozszerzeniem .xlsx lub .xls");
+      setPleaseWait(false);
+
+      return;
+    }
+    try {
+      setPleaseWait(true);
+      const formData = new FormData();
+      formData.append("excelFile", file);
+
+   const raport=   await axiosPrivateIntercept.post(
+        `/add-data/send-documents/${type}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            
+          },
+              responseType: "arraybuffer", // <- bardzo ważne
+        }
+      );
+         const blob = new Blob([raport.data], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+
+    // pobieramy plik
+    saveAs(blob, "raport.xlsx");
       if (type === "becared") {
         setErrBecared("Dokumenty zaktualizowane.");
       }
@@ -116,14 +173,14 @@ const AddDataFromFile = () => {
             </section>
           )}
 
-          {/* {!errRandomFile ? (
+          {!errRandomFile ? (
             <section className="add_data_from_file__container-documents">
               <input
                 type="file"
                 name="uploadfile"
                 id="random"
                 style={{ display: "none" }}
-                onChange={(e) => handleSendFileBL(e, "random")}
+                onChange={(e) => handleSendFileBL2(e, "random")}
               />
               <label htmlFor="random" className="add_data_file-click-me">
                 Przesyłanie pliku jednorazowego
@@ -133,7 +190,7 @@ const AddDataFromFile = () => {
             <section className="add_data_from_file__container-documents">
               <span className="add_data_file-click-me">{errRandomFile}</span>
             </section>
-          )} */}
+          )}
         </section>
       </section>
 
