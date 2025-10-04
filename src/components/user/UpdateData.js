a3bd828c24e0123134ab44bc56ca7803af7e5b6a
jerzyk2007 <jerzykomorowski77@gmail.com>
2025-10-04 14:28:44 +0200

    porządek

--- src/components/user/UpdateData.js
+++ src/components/user/UpdateData.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import './UpdateData.css';
+import "./UpdateData.css";
 
 const UpdateData = ({ data }) => {
-    const updateItems = data.map((item, index) => {
-        return (
-            <section key={index} className='update-data__container'>
-                <span className='update-data--name'>{item.DATA_NAME}</span>
-                <span className='update-data--date'>{item.DATE}</span>
-                <span className='update-data--hour'>{item.HOUR}</span>
-                <span className='update-data--update'>{item.UPDATE_SUCCESS}</span>
-
-            </section>
-        );
-    });
-
+  const updateItems = data.map((item, index) => {
     return (
-        <section className='update-data'>
-            <section className='update-data__container'>
-                <span className='update-data--name'>Nazwa:</span>
-                <span className='update-data--date'>Data:</span>
-                <span className='update-data--hour'>Godzina:</span>
-                <span className='update-data--update'>Status:</span>
-            </section>
-            {updateItems}
-
-        </section>
+      <section key={index} className="update-data__container">
+        <span className="update-data--name">{item.DATA_NAME}</span>
+        <span className="update-data--date">{item.DATE}</span>
+        <span className="update-data--hour">{item.HOUR}</span>
+        <span className="update-data--update">{item.UPDATE_SUCCESS}</span>
+      </section>
     );
+  });
+
+  return (
+    <section className="update-data">
+      <section className="update-data__container">
+        <span className="update-data--name">Nazwa:</span>
+        <span className="update-data--date">Data:</span>
+        <span className="update-data--hour">Godzina:</span>
+        <span className="update-data--update">Status:</span>
+      </section>
+      {updateItems}
+    </section>
+  );
 };
 
 export default UpdateData;
