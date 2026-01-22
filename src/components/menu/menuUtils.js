f0640c74aae3858cc6936dfea1bb3ef50136eae3
jerzyk2007 <jerzykomorowski77@gmail.com>
2026-01-22 17:25:52 +0100

    porządek

--- src/components/menu/menuUtils.js
+++ src/components/menu/menuUtils.js
@@ -1,33 +1,3 @@
-// // menuUtils.js lub menuConfig.js
-// import { menuItems } from "./menuConfig";
-
-// export const getSecurityForPath = (path) => {
-//   // Funkcja pomocnicza do przeszukiwania rekurencyjnego
-//   const findItem = (items) => {
-//     for (const item of items) {
-//       // Jeśli ścieżka się zgadza, zwróć zabezpieczenia
-//       if (item.path === path) {
-//         return { roles: item.roles || [], company: item.company || [] };
-//       }
-//       // Jeśli ma submenu, szukaj głębiej
-//       if (item.submenu) {
-//         const found = findItem(item.submenu);
-//         if (found) return found;
-//       }
-//       // Jeśli ma sideMenu, szukaj głębiej
-//       if (item.sideMenu) {
-//         const found = findItem(item.sideMenu);
-//         if (found) return found;
-//       }
-//     }
-//     return null;
-//   };
-//   console.log(findItem(menuItems));
-
-//   return findItem(menuItems);
-// };
-
-// menuUtils.js
 import { menuItems } from "./menuConfig";
 
 export const getSecurityForPath = (path) => {
